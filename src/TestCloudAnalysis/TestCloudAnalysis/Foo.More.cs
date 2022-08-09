a9384bc0de6d6a92a31374be88038b76df8b03c2
Costin Zaharia <costin.zaharia@sonarsource.com>
2022-08-09 11:06:39 +0200

    Add proj

---
+++ src/TestCloudAnalysis/TestCloudAnalysis/Foo.More.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TestCloudAnalysis;
+
+public partial class Foo
+{
+    private static bool Validation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        return false;
+    }
+}
---
+++ src/TestCloudAnalysis/TestCloudAnalysis/Foo.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace TestCloudAnalysis;
+
+public partial class Foo
+{
+    public void Call()
+    {
+        var webRequest = (HttpWebRequest)WebRequest.Create("http://localhost");
+
+        webRequest.ServerCertificateValidationCallback += Validation;
+    }
+}
